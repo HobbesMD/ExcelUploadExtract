065eb58057cc6bf1414e96942ace27a75a6aa169
Michael Dykema <mbdykema@gmail.com>
2025-06-22 15:38:38 -0400

    Add API project

---
+++ CompanyProject/ExcelUploadExtract.API/Models/SalesLead.cs
@@ -0,0 +1,10 @@
+﻿namespace ExcelUploadExtract.API.Models
+{
+    public class SalesLead
+    {
+        public required DateTime QuoteSentDate { get; set; }
+        public required string SalesPerson { get; set; }
+        public required string ProjectName { get; set; }
+        public required double QuoteAmount { get; set; }
+    }
+}
